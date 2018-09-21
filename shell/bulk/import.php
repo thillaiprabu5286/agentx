79645b0d0dca749fd484a4afa28f0e8ad027ab3c
Prabu Goodhope <prabu@goodhope.ae>
2018-09-21 17:22:21 +0530

    Bug fix

--- shell/bulk/import.php
+++ shell/bulk/import.php
@@ -34,27 +34,68 @@ class Dever_Shell_Import_Import extends Mage_Shell_Abstract
     {
         ini_set('memory_limit', '2G');
         $this->saveProductOptions();
+        $this->prepareDataForImport();
         $this->saveProduct();
     }
 
     public function saveProductOptions()
+    {
+        $debug = true;
+        /** @var Dever_Import_Model_Import $model */
+        $model = Mage::getModel('dever_import/import');
+        try {
+            if ($this->_processData) {
+                $csvHeaders = array();
+                echo "--Prepare Product options ...\n";
+                $i = 1;
+                foreach ($this->_processData as $key => $lines) {
+                    if ($key == 0) {
+                        $csvHeaders = $lines;
+                    } else {
+                        $arrayCombined = array_combine($csvHeaders, $lines);
+                        //print_r($arrayCombined);
+                        //exit;
+                        $model->saveProductOptions($arrayCombined);
+                    }
+                    echo "Row {$i} \n";
+                    $i++;
+                }
+                echo "--End Product options ...\n";
+
+            }
+        } catch (Exception $e) {
+            echo (string)$e->getMessage();
+        }
+    }
+
+    public function prepareDataForImport()
     {
         /** @var Dever_Import_Model_Import $model */
         $model = Mage::getModel('dever_import/import');
         try {
             if ($this->_processData) {
                 $csvHeaders = array();
-                echo "--Prepare Product Options ...\n";
-                foreach ($this->_processData as $key => $lines) {
+                $importData = array();
+                echo "--Prepare Product key pair ...\n";
+                $i = 1;
+                foreach ($this->_processData as $key => $lines)
+                {
+                    //print_r($lines);
                     if ($key == 0) {
                         $csvHeaders = $lines;
                     } else {
+                        //print_r($csvHeaders);
                         $arrayCombined = array_combine($csvHeaders, $lines);
-                        $model->saveProductOptions($arrayCombined);
+                        $importData[] = $model->prepareDataForImport($arrayCombined);
                     }
+                    echo "Row {$i} \n";
+                    $i++;
+                    //print_r($arrayCombined);
+                    //exit;
                 }
-                echo "--End Product Options ...\n";
-
+                echo "--End Product Key pair ...\n";
+                //exit;
+                $this->_importData = $importData;
             }
         } catch (Exception $e) {
             echo (string)$e->getMessage();
@@ -63,24 +104,21 @@ class Dever_Shell_Import_Import extends Mage_Shell_Abstract
 
     public function saveProduct()
     {
-        /** @var Dever_Import_Model_Import $model */
-        $model = Mage::getModel('dever_import/import');
         try {
-            if ($this->_processData) {
-                $csvHeaders = array();
-                $importData = array();
-                echo "-- Prepare Product Save ...\n";
-                foreach ($this->_processData as $key => $lines)
+            if ($importData = $this->_importData) {
+                /** @var Dever_Import_Model_Import $model */
+                $model = Mage::getModel('dever_import/import');
+                echo "--Prepare Product Save ...\n";
+                $i = 1;
+                foreach ($importData as $data)
                 {
-                    if ($key == 0) {
-                        $csvHeaders = $lines;
-                    } else {
-                        $data = array_combine($csvHeaders, $lines);
-                        $model->saveProduct($data, $this->getArg('mediaDir'));
-                    }
+                    $model->saveProduct($data, $this->getArg('mediaDir'));
+                    echo "Row {$i} \n";
+                    $i++;
+
                 }
-                echo "-- End Product Save ....\n";
-                $this->_importData = $importData;
+                echo "--End Product Save ...\n";
+
             }
         } catch (Exception $e) {
             echo (string)$e->getMessage();

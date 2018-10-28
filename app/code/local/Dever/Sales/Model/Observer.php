036ba6ce4d03a7df3404de162a0aaec8c5fedd9e
prabu <prabusmiles05@gmail.com>
2018-10-28 19:49:23 +0530

    Fcm looping

--- app/code/local/Dever/Sales/Model/Observer.php
+++ app/code/local/Dever/Sales/Model/Observer.php
@@ -4,42 +4,43 @@ class Dever_Sales_Model_Observer
 {
     public function triggerFcm($observer)
     {
+        $debug = true;
         $order = $observer->getEvent()->getOrder();
-
         /** @var Dever_Sms_Helper_Fcm $helper */
         $helper = Mage::helper('dever_sms/fcm');
-
         $customerId = $order->getCustomerId();
         $customer = Mage::getModel('customer/customer')->load($customerId);
-        $fcmId = $customer->getFcmId();
-        $status = uc_words($order->getStatus());
-
-        switch ($order->getStatus()) {
-            case 'pending':
-                $message = "Dear {$customer->getName()}, Thanks for your Order. Your Order {$order->getIncrementId()} is submitted with AgentX team. 
+        $list = $customer->getFcmId();
+        $listArr = explode(',',$list);
+        foreach ($listArr as $fcmId) {
+            $status = uc_words($order->getStatus());
+            switch ($order->getStatus()) {
+                case 'pending':
+                    $message = "Dear {$customer->getName()}, Thanks for your Order. Your Order {$order->getIncrementId()} is submitted with AgentX team. 
                 We will get back to you shortly.";
-                break;
-            case 'accepted':
-            case 'complete':
-            case 'canceled':
-                $message = "Dear {$customer->getName()}, Your Order {$order->getIncrementId()} is currently in {$status} status.";
-                break;
-            default:
-                //Do Nothing
-        }
-
-        if ($message) {
-            $helper->sendSms($fcmId, $message);
-            //Trigger Notification Event to log messages
-            $notification = array(
-                'fcm_id' => $fcmId,
-                'customer_id' => $customerId,
-                'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
-                'email' => $customer->getEmail(),
-                'message' => $message,
-                'created_date' => date('Y-m-d H:i:s')
-            );
-            Mage::dispatchEvent('log_notification_messages', array('notification' => $notification));
+                    $helper->sendSms($fcmId, $message);
+                    break;
+                case 'accepted':
+                case 'complete':
+                case 'canceled':
+                    $message = "Dear {$customer->getName()}, Your Order {$order->getIncrementId()} is currently in {$status} status.";
+                    $helper->sendSms($fcmId, $message);
+                    break;
+                default:
+                    //Do Nothing
+            }
+            if ($message != '' && isset($message)) {
+                //Trigger Notification Event to log messages
+                $notification = array(
+                    'fcm_id' => $fcmId,
+                    'customer_id' => $customerId,
+                    'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
+                    'email' => $customer->getEmail(),
+                    'message' => $message,
+                    'created_date' => date('Y-m-d H:i:s')
+                );
+                Mage::dispatchEvent('log_notification_messages', array('notification' => $notification));
+            }
         }
     }
 }

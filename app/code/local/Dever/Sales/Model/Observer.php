f2e9c6b72a97facbffe67211476314e2918cbb57
prabu <prabusmiles05@gmail.com>
2018-10-30 10:56:24 +0530

    Change in event

--- app/code/local/Dever/Sales/Model/Observer.php
+++ app/code/local/Dever/Sales/Model/Observer.php
@@ -2,18 +2,57 @@
 
 class Dever_Sales_Model_Observer
 {
-    public function triggerFcm($observer)
+    public function triggerFcmNewOrder($observer)
     {
-        $debug = true;
         $order = $observer->getEvent()->getOrder();
+        $status = $order->getStatus();
+        /** @var Dever_Sms_Helper_Fcm $helper */
+        $helper = Mage::helper('dever_sms/fcm');
+        $customerId = $order->getCustomerId();
+        $customer = Mage::getModel('customer/customer')->load($customerId);
+        $list = $customer->getFcmId();
+        $listArr = explode(',',$list);
+        foreach ($listArr as $fcmId) {
+            $status = uc_words($status);
+            switch ($status) {
+                case 'pending':
+                    $message = "Dear {$customer->getName()}, Thanks for your Order. Your Order {$order->getIncrementId()} is submitted with AgentX team. 
+                We will get back to you shortly.";
+                    $helper->sendSms($fcmId, $message);
+                    break;
+                case 'accepted':
+                case 'complete':
+                case 'canceled':
+                    $message = "Dear {$customer->getName()}, Your Order {$order->getIncrementId()} is currently in {$status} status.";
+                    $helper->sendSms($fcmId, $message);
+                    break;
+                default:
+                    //Do Nothing
+            }
+            if ($message != '' && isset($message)) {
+                //Trigger Notification Event to log messages
+                $notification = array(
+                    'fcm_id' => $fcmId,
+                    'customer_id' => $customerId,
+                    'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
+                    'email' => $customer->getEmail(),
+                    'message' => $message,
+                    'created_date' => date('Y-m-d H:i:s')
+                );
+                Mage::dispatchEvent('log_notification_messages', array('notification' => $notification));
+            }
+        }
+    }
 
+    public function triggerFcmStatusChange($observer)
+    {
+        $order = $observer->getEvent()->getOrder();
         $status = $order->getStatus();
         $originalData = $order->getOrigData();
         $previousStatus = $originalData['status'];
         if ($status == $previousStatus) {
             return;
         }
-
         /** @var Dever_Sms_Helper_Fcm $helper */
         $helper = Mage::helper('dever_sms/fcm');
         $customerId = $order->getCustomerId();

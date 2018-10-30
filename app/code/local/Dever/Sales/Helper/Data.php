7ce4ea95b205f4f88da60306b8f8f938f97a03e7
prabu <prabusmiles05@gmail.com>
2018-10-30 16:38:08 +0530

    Remove loop

--- app/code/local/Dever/Sales/Helper/Data.php
+++ app/code/local/Dever/Sales/Helper/Data.php
@@ -4,32 +4,29 @@ class Dever_Sales_Helper_Data extends Mage_Core_Helper_Abstract
 {
     public function triggerFcm($order)
     {
-        Mage::log("Start Fcm Trigger",null,"fcm.log");
+        //Mage::log("Start Fcm Trigger",null,"fcm.log");
         $status = $order->getStatus();
         /** @var Dever_Sms_Helper_Fcm $helper */
         $helper = Mage::helper('dever_sms/fcm');
         $customerId = $order->getCustomerId();
         $customer = Mage::getModel('customer/customer')->load($customerId);
-        $list = $customer->getFcmId();
-        $listArr = explode(',',$list);
-        foreach ($listArr as $fcmId) {
-            Mage::log("Process Fcm ID:" .$fcmId,null,"fcm.log");
-            $message = "Dear {$customer->getName()}, Thanks for your Order. Your Order {$order->getIncrementId()} is submitted with AgentX team. 
+        $fcmId = $customer->getFcmId();
+        //Mage::log("Process Fcm ID:" .$fcmId,null,"fcm.log");
+        $message = "Dear {$customer->getName()}, Thanks for your Order. Your Order {$order->getIncrementId()} is submitted with AgentX team. 
                 We will get back to you shortly.";
-            Mage::log("Process Status:" . $status,null,"fcm.log");
-            Mage::log("Process Message:" . $message,null,"fcm.log");
-            $helper->sendSms($fcmId, $message);
-            //Trigger Notification Event to log messages
-            $notification = array(
-                'fcm_id' => $fcmId,
-                'customer_id' => $customerId,
-                'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
-                'email' => $customer->getEmail(),
-                'message' => $message,
-                'created_date' => date('Y-m-d H:i:s')
-            );
-            Mage::dispatchEvent('log_notification_messages', array('notification' => $notification));
-        }
-        Mage::log("End Fcm Trigger",null,"fcm.log");
+        //Mage::log("Process Status:" . $status,null,"fcm.log");
+        //Mage::log("Process Message:" . $message,null,"fcm.log");
+        $helper->sendSms($fcmId, $message);
+        //Trigger Notification Event to log messages
+        $notification = array(
+            'fcm_id' => $fcmId,
+            'customer_id' => $customerId,
+            'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
+            'email' => $customer->getEmail(),
+            'message' => $message,
+            'created_date' => date('Y-m-d H:i:s')
+        );
+        Mage::dispatchEvent('log_notification_messages', array('notification' => $notification));
+        //Mage::log("End Fcm Trigger",null,"fcm.log");
     }
 }
--- app/code/local/Dever/Sales/Model/Observer.php
+++ app/code/local/Dever/Sales/Model/Observer.php
@@ -12,30 +12,27 @@ class Dever_Sales_Model_Observer
         if ($status == $previousStatus) {
             return;
         }
-        Mage::log("Start Fcm Trigger",null,"fcm.log");
+        //Mage::log("Start Fcm Trigger",null,"fcm.log");
         /** @var Dever_Sms_Helper_Fcm $helper */
         $helper = Mage::helper('dever_sms/fcm');
         $customerId = $order->getCustomerId();
         $customer = Mage::getModel('customer/customer')->load($customerId);
-        $list = $customer->getFcmId();
-        $listArr = explode(',',$list);
-        foreach ($listArr as $fcmId) {
-            Mage::log("Process Fcm ID:" .$fcmId,null,"fcm.log");
-            Mage::log("Old Status:" . $previousStatus,null,"fcm.log");
-            $message = "Dear {$customer->getName()}, Your Order {$order->getIncrementId()} is currently in {$status} status.";
-            Mage::log("New Status:" . $status,null,"fcm.log");
-            $helper->sendSms($fcmId, $message);
-            //Trigger Notification Event to log messages
-            $notification = array(
-                'fcm_id' => $fcmId,
-                'customer_id' => $customerId,
-                'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
-                'email' => $customer->getEmail(),
-                'message' => $message,
-                'created_date' => date('Y-m-d H:i:s')
-            );
-            Mage::dispatchEvent('log_notification_messages', array('notification' => $notification));
-        }
-        Mage::log("End Fcm Trigger",null,"fcm.log");
+        $fcmId = $customer->getFcmId();
+        //Mage::log("Process Fcm ID:" .$fcmId,null,"fcm.log");
+        //Mage::log("Old Status:" . $previousStatus,null,"fcm.log");
+        $message = "Dear {$customer->getName()}, Your Order {$order->getIncrementId()} is currently in {$status} status.";
+        //Mage::log("New Status:" . $status,null,"fcm.log");
+        $helper->sendSms($fcmId, $message);
+        //Trigger Notification Event to log messages
+        $notification = array(
+            'fcm_id' => $fcmId,
+            'customer_id' => $customerId,
+            'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
+            'email' => $customer->getEmail(),
+            'message' => $message,
+            'created_date' => date('Y-m-d H:i:s')
+        );
+        Mage::dispatchEvent('log_notification_messages', array('notification' => $notification));
+        //Mage::log("End Fcm Trigger",null,"fcm.log");
     }
 }

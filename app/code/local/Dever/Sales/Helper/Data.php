13c9a265c0f117143d834dc4c50c3b947d0f5721
Thillai Prabu <prabusmiles05@gmail.com>
2019-02-13 09:54:09 +0530

    Order create fcm

--- app/code/local/Dever/Sales/Helper/Data.php
+++ app/code/local/Dever/Sales/Helper/Data.php
@@ -8,23 +8,31 @@ class Dever_Sales_Helper_Data extends Mage_Core_Helper_Abstract
         $helper = Mage::helper('dever_sms/fcm');
         $customerId = $order->getCustomerId();
         $customer = Mage::getModel('customer/customer')->load($customerId);
-        $fcmId = $customer->getFcmId();
-        //Mage::log("Process Fcm ID:" .$fcmId,null,"fcm.log");
         $message = "Dear {$customer->getName()}, Thanks for your Order. Your Order {$order->getIncrementId()} is submitted with AgentX team. 
                 We will get back to you shortly.";
-        //Mage::log("Process Status:" . $status,null,"fcm.log");
-        //Mage::log("Process Message:" . $message,null,"fcm.log");
-        $helper->sendSms($fcmId, $message);
-        //Trigger Notification Event to log messages
-        $notification = array(
-            'fcm_id' => $fcmId,
-            'customer_id' => $customerId,
-            'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
-            'email' => $customer->getEmail(),
-            'message' => $message,
-            'created_date' => date('Y-m-d H:i:s', strtotime($order->getCreatedAt(). ' + 240 mins'))
-        );
-        Mage::dispatchEvent('log_notification_messages', array('notification' => $notification));
-        //Mage::log("End Fcm Trigger",null,"fcm.log");
+
+        //Multi Fcm logic starts here
+        $notificationList = array();
+
+        /** @var Dever_Customer_Model_Fcm $model */
+        $model = Mage::getModel('dever_customer/fcm');
+        $fcmIds = $model->filterByCustomer($customerId);
+        foreach ($fcmIds as $each)
+        {
+            $helper->sendSms($each, $message);
+            //Trigger Notification Event to log messages
+            $notificationList[] = array(
+                'fcm_id' => $each,
+                'customer_id' => $customerId,
+                'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
+                'email' => $customer->getEmail(),
+                'message' => $message,
+                'created_date' => date('Y-m-d H:i:s', strtotime($order->getCreatedAt(). ' + 240 mins'))
+            );
+        }
+
+        Mage::dispatchEvent('log_notification_messages', array('notification' => $notificationList));
+        //Multi Fcm logic ends here
+
     }
 }

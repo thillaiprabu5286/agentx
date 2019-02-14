7384375c13970b303c7048285bb5d9da79068acf
Thillai Prabu <prabusmiles05@gmail.com>
2019-02-14 09:22:24 +0530

    removed unnecessary model

--- app/code/local/Dever/Sales/Helper/Data.php
+++ app/code/local/Dever/Sales/Helper/Data.php
@@ -6,9 +6,9 @@ class Dever_Sales_Helper_Data extends Mage_Core_Helper_Abstract
     {
         /** @var Dever_Sms_Helper_Fcm $helper */
         $helper = Mage::helper('dever_sms/fcm');
-        $customerId = $order->getCustomerId();
-        $customer = Mage::getModel('customer/customer')->load($customerId);
-        $message = "Dear {$customer->getName()}, Thanks for your Order. Your Order {$order->getIncrementId()} is submitted with AgentX team. 
+
+        $name = $order->getCustomerFirstname() . ' ' . $order->getCustomerLastname();
+        $message = "Dear {$name}, Thanks for your Order. Your Order {$order->getIncrementId()} is submitted with AgentX team. 
                 We will get back to you shortly.";
 
         //Multi Fcm logic starts here
@@ -16,16 +16,16 @@ class Dever_Sales_Helper_Data extends Mage_Core_Helper_Abstract
 
         /** @var Dever_Customer_Model_Fcm $model */
         $model = Mage::getModel('dever_customer/fcm');
-        $fcmIds = $model->filterByCustomer($customerId);
+        $fcmIds = $model->filterByCustomer($order->getCustomerId());
         foreach ($fcmIds as $each)
         {
             $helper->sendSms($each, $message);
             //Trigger Notification Event to log messages
             $notificationList[] = array(
                 'fcm_id' => $each,
-                'customer_id' => $customerId,
-                'name' => $customer->getFirstname() . ' ' . $customer->getLastname(),
-                'email' => $customer->getEmail(),
+                'customer_id' => $order->getCustomerId(),
+                'name' => $name,
+                'email' => $order->getCustomerEmail(),
                 'message' => $message,
                 'created_date' => date('Y-m-d H:i:s', strtotime($order->getCreatedAt(). ' + 240 mins'))
             );

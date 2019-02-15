040c3559442458fc9dd267f43f5848f36ffb98e0
Thillai Prabu <prabusmiles05@gmail.com>
2019-02-15 20:55:17 +0530

    Bulk order script

---
+++ shell/bulk/orders.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prabu
+ * Date: 20/11/17
+ * Time: 5:55 PM
+ */
+
+require_once '../abstract.php';
+
+require_once '../simplexlsx.class.php';
+
+class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
+{
+    protected $_processData = null;
+
+    public function _construct()
+    {
+        parent::_construct();
+        $datafile = Mage::getBaseDir('var') . DS . 'import' . DS . 'orders.xlsx';
+
+        echo "Loading {$datafile}. \n";
+        $xlsx = @(new SimpleXLSX($datafile));
+        $rows =  $xlsx->rows();
+        $total = count($rows);
+        echo "Loaded {$total} rows. \n";
+
+        $this->_processData = $rows;
+    }
+
+    public function run()
+    {
+        ini_set('memory_limit', '2G');
+
+        $orderData = $this->prepareData();
+        $this->createOrder($orderData);
+    }
+
+    /**
+     * Create Sales Order
+     *
+     * @param $orderData
+     */
+    public function prepareData()
+    {
+        $orderData = array();
+        try {
+            if ($this->_processData) {
+                $csvHeaders = array();
+                echo "--Prepare Save ...\n";
+                foreach ($this->_processData as $key => $lines) {
+                    if ($key == 0) {
+                        $csvHeaders = $lines;
+                    } else {
+                        $orderData = array_combine($csvHeaders, $lines);
+                    }
+                }
+                echo "--End Save ...\n";
+
+            }
+        } catch (Exception $e) {
+            echo (string)$e->getMessage();
+        }
+
+        return $orderData;
+    }
+
+    public function createOrder($orderData)
+    {
+        $customer = Mage::getModel('customer/customer')
+            ->setWebsiteId(2)
+            ->loadByEmail($orderData['email']);
+
+        //Prepare from Sheet Data as accepted Order array
+        $orderData = array (
+            'session' => array (
+                'customer_id' => $customer->getId(),
+                'store_id' => 0
+            ),
+            'payment' => array (
+                'method' => 'cashondelivery'
+            ),
+            'items' => $this->_buildItems($orderData['items']),
+            'order' => array (
+                'currency' => 'AED',
+                'billing_address' => array (
+                    'firstname' => $orderData['firstname'],
+                    'lastname' => $orderData['lastname'],
+                    'street' => $orderData['address'],
+                    'country_id' => $orderData['country'],
+                    'city' => $orderData['city'],
+                    'postcode' => $orderData['postcode'],
+                    'telephone' => $orderData['telephone']
+                )
+            ),
+            'shipping_method' => 'freeshipping_freeshipping',
+            'comment' => 'Order Created using sheet'
+        );
+
+        if (!empty($orderData)) {
+
+            $this->_initSession($orderData['session']);
+
+            try {
+                $this->_processQuote($orderData);
+                if (!empty($orderData['payment'])) {
+                    $this->_getOrderCreateModel()->setPaymentData($orderData['payment']);
+                    $this->_getOrderCreateModel()->getQuote()->getPayment()->addData($orderData['payment']);
+                }
+
+                $order = $this->_getOrderCreateModel()
+                    ->importPostData($orderData['order'])
+                    ->createOrder();
+
+                echo "{$order->getIncrementId()} \n";
+
+                $this->_getSession()->clear();
+
+            } catch (Exception $e){
+                echo (string)$e->getMessage();
+            }
+        }
+    }
+
+    protected function _buildItems($items)
+    {
+        $itemArr = array();
+        $splitItems = explode(',', $items);
+        foreach ($splitItems as $split)
+        {
+            $each = explode(':', $split);
+            foreach ($each as $qty => $sku)
+            {
+                $productId = $this->_getProduct()->getIdBySku($sku);
+                $itemArr[$productId] = array ('qty' => $qty);
+            }
+        }
+
+        return $itemArr;
+    }
+    /**
+     * Prepare and Process Quote for Sales Order Creation
+     *
+     * @param array $data
+     * @return $this
+     */
+    protected function _processQuote($data = array())
+    {
+        /* Saving order data */
+        if (!empty($data['order'])) {
+            $this->_getOrderCreateModel()->importPostData($data['order']);
+        }
+
+        $this->_getOrderCreateModel()->getBillingAddress();
+        $this->_getOrderCreateModel()->setShippingAsBilling(true);
+
+        /* Add Product */
+        if (!empty($data['items'])) {
+            $this->_getOrderCreateModel()->addProducts($data['items']);
+        }
+
+        /* Collect shipping rates */
+        $this->_getOrderCreateModel()->collectShippingRates();
+
+        /* Add payment data */
+        if (!empty($data['payment'])) {
+            $this->_getOrderCreateModel()->getQuote()->getPayment()->addData($data['payment']);
+        }
+
+        $this->_getOrderCreateModel()
+            ->initRuleData()
+            ->saveQuote();
+
+        if (!empty($data['payment'])) {
+            $this->_getOrderCreateModel()->getQuote()->getPayment()->addData($data['payment']);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Initialize order creation session data
+     *
+     * @param $data
+     * @return $this
+     */
+    protected function _initSession($data)
+    {
+        /* Get/identify customer */
+        if (!empty($data['customer_id'])) {
+            $this->_getSession()->setCustomerId((int) $data['customer_id']);
+        }
+
+        /* Get/identify store */
+        if (!empty($data['store_id'])) {
+            $this->_getSession()->setStoreId((int) $data['store_id']);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Retrieve order create model
+     *
+     * @return  Mage_Adminhtml_Model_Sales_Order_Create
+     */
+    protected function _getOrderCreateModel()
+    {
+        return Mage::getSingleton('adminhtml/sales_order_create');
+    }
+
+    /**
+     * Retrieve session object
+     *
+     * @return Mage_Adminhtml_Model_Session_Quote
+     */
+    protected function _getSession()
+    {
+        return Mage::getSingleton('adminhtml/session_quote');
+    }
+
+    /**
+     * Retrieve Product Model
+     *
+     * @return Mage_Catalog_Model_Product
+     */
+    protected function _getProduct()
+    {
+        return Mage::getModel('catalog/product');
+    }
+
+}
+
+$obj = new Dever_Shell_Bulk_Orders();
+$obj->run();

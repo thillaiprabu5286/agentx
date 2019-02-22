d904af2f501ca8483e54ea5d7e35fb4d86c51143
Thillai Prabu <prabusmiles05@gmail.com>
2019-02-22 09:34:04 +0530

    Bulk orders script / Upload UI

--- shell/bulk/orders.php
+++ shell/bulk/orders.php
@@ -14,16 +14,20 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
 {
     protected $_processData = null;
 
+    const DEFAULT_WEBSITE = 2;
+
+    const DEFAULT_STORE = 2;
+
     public function _construct()
     {
         parent::_construct();
         $datafile = Mage::getBaseDir('var') . DS . 'import' . DS . 'orders.xlsx';
 
-        echo "Loading {$datafile}. \n";
+        //echo "Loading {$datafile}. \n";
         $xlsx = @(new SimpleXLSX($datafile));
         $rows =  $xlsx->rows();
         $total = count($rows);
-        echo "Loaded {$total} rows. \n";
+        //echo "Loaded {$total} rows. \n";
 
         $this->_processData = $rows;
     }
@@ -47,16 +51,13 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
         try {
             if ($this->_processData) {
                 $csvHeaders = array();
-                echo "--Prepare Save ...\n";
                 foreach ($this->_processData as $key => $lines) {
                     if ($key == 0) {
                         $csvHeaders = $lines;
                     } else {
-                        $orders = array_combine($csvHeaders, $lines);
+                        $orders[] = array_combine($csvHeaders, $lines);
                     }
                 }
-                echo "--End Save ...\n";
-
             }
         } catch (Exception $e) {
             echo (string)$e->getMessage();
@@ -67,18 +68,35 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
 
     public function createOrder($orders)
     {
-        if (!empty($orders)) {
-            foreach ($orders as $orderData) {
+        if (!empty($orders) && isset($orders)) {
+
+            //print_r($orders);
+            //exit;
+
+            Mage::log("Job Started - " . date('Y-m-d H:i:s'), null, 'bulkimport.log');
+            $i = 1;
+            foreach ($orders as $orderData)
+            {
+                if (empty($orderData['email'])) {
+                    Mage::log("\t Row #{$i} Skip Row - Customer Email is empty", null, "bulkimport.log");
+                    continue;
+                }
 
                 $customer = Mage::getModel('customer/customer')
-                    ->setWebsiteId(2)
+                    ->setWebsiteId(self::DEFAULT_WEBSITE)
                     ->loadByEmail($orderData['email']);
+
+                if (empty($customer->getId())) {
+                    //Mage::throwException('Customer does not Exists - Skip Row');
+                    Mage::log("\t Row #{$i} Customer Email " . $orderData['email'] . " does not exists", null, "bulkimport.log");
+                    continue;
+                }
 
                 //Prepare from Sheet Data as accepted Order array
                 $orderData = array (
                     'session' => array (
                         'customer_id' => $customer->getId(),
-                        'store_id' => 0
+                        'store_id' => self::DEFAULT_STORE
                     ),
                     'payment' => array (
                         'method' => 'cashondelivery'
@@ -86,7 +104,7 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
                     'items' => $this->_buildItems($orderData['items']),
                     'order' => array (
                         'currency' => 'AED',
-                        'billing_address' => array (
+                        'shipping_address' => array (
                             'firstname' => $orderData['firstname'],
                             'lastname' => $orderData['lastname'],
                             'street' => $orderData['address'],
@@ -113,14 +131,18 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
                         ->importPostData($orderData['order'])
                         ->createOrder();
 
-                    echo "{$order->getIncrementId()} \n";
+                    //echo "{$order->getIncrementId()} \n";
+                    Mage::log("\t Row #{$i} Order Success - " . $order->getIncrementId(), null, 'bulkimport.log');
 
                     $this->_getSession()->clear();
 
                 } catch (Exception $e){
-                    echo (string)$e->getMessage();
+                    Mage::logException($e);
                 }
+                $i++;
             }
+
+            Mage::log("Job End - " . date('Y-m-d H:i:s'), null, 'bulkimport.log');
         }
     }
 
@@ -130,11 +152,10 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
         $splitItems = explode(',', $items);
         foreach ($splitItems as $split)
         {
-            $each = explode(':', $split);
-            foreach ($each as $qty => $sku)
-            {
-                $productId = $this->_getProduct()->getIdBySku($sku);
-                $itemArr[$productId] = array ('qty' => $qty);
+            $val = explode(':', $split);
+            $productId = $this->_getProduct()->getIdBySku($val[0]);
+            if (isset($productId) && !empty($productId)) {
+                $itemArr[$productId] = $val[1];
             }
         }
 
@@ -194,13 +215,15 @@ class Dever_Shell_Bulk_Orders extends Mage_Shell_Abstract
         }
 
         /* Get/identify store */
-        if (!empty($data['store_id'])) {
-            $this->_getSession()->setStoreId((int) $data['store_id']);
+        $this->_getSession()->setStoreId(self::DEFAULT_STORE);
+
+        /* Get/identify store */
+        if (!empty($data['quote_id'])) {
+            $this->_getSession()->setQuoteId((int) $data['quote_id']);
         }
 
         return $this;
     }
-
     /**
      * Retrieve order create model
      *
